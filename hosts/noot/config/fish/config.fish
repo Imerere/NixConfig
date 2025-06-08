f4b98aa7d8c40ac663d9e585b1675af12a3925e7
Imerere <char.tsi@proton.me>
2025-06-08 02:30:44 +0100

    updated to exclude pywal in noot

--- hosts/noot/config/fish/config.fish
+++ hosts/noot/config/fish/config.fish
@@ -1,11 +1,7 @@
 if status is-interactive
-    cat /home/imere/.cache/wal/sequences
     set fish_greeting
     alias cider "appimage-run ~/Music/cider-v3.0.2-linux-x64.AppImage"
     clear
     fastfetch
     starship init fish | source
-    function fish_postexec --on-event fish_postexec
-        cat ~/.cache/wal/sequences
-    end
 end
